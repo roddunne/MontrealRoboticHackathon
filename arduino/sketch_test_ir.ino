7b769ee4e8c86a259a1d82ce0d0f17ac196fa266
Rod Dunne <rdunne@rim.com>
2012-03-03 13:08:13 -0500

    ir test code

---
+++ arduino/sketch_test_ir.ino
@@ -0,0 +1,17 @@
+void setup()
+{
+Serial.begin(115200); //Set serial baud rate to 115200
+
+pinMode(A0,LOW);
+pinMode(A1,HIGH);
+
+}
+
+void loop()
+{
+      int val;
+      val=analogRead(2);   //connect mic sensor to Analog 0
+      Serial.println(val,DEC);//print the sound value to serial        
+      delay(100);
+}
+
